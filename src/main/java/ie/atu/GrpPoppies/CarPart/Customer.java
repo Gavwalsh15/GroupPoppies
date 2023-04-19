ecfa2d755d03dc6fec1b8845f3795ef87af8537e
Rakib <G00408644@atu.ie>
2023-04-19 22:29:38 +0100

    Added Customer Class with variable name, email and number with getter and setters

---
+++ src/main/java/ie/atu/GrpPoppies/CarPart/Customer.java
@@ -0,0 +1,37 @@
+package ie.atu.GrpPoppies.CarPart;
+
+public class Customer {
+    private String name;
+    private String email;
+    private int number;
+
+    public Customer(String name, String email, int number) {
+        this.name = name;
+        this.email = email;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
